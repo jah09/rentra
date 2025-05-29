b2db375cbe701a7887b7eaff55c58f84af78223d
jah09 <jasuhot1@gmail.com>
2025-05-29 23:51:21 +0800

    fix: correct text content in HomeScreen component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ export default function HomeScreen() {
   return (
     <View className="text-green-500">
       <Text className="mt-10 text-red-900 font-regular text-lg"> This is sample1!</Text>
-      <Text className="mt-10 text-red-900 font-regular"> This is sample12!</Text>
+      <Text className="mt-10 text-red-900 font-regular"> This is sample12!!</Text>
 
       <Button className="mt-10" variant="default" size={"lg"}>
         <Text className="font-pbold">Press Mea</Text>
